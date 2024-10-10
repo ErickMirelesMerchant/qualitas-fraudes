0bc7630008930a81d48017fb3aa2ffcc77c41990
ErickMirelesMerchant <134002546+ErickMirelesMerchant@users.noreply.github.com>
2024-10-09 19:25:59 -0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: "src/", 
   ssr: true,
-  app: {
+  app: 
+    baseURL: '/qualitas-fraudes/',
     head: {
       title: "Qualitas Fraudes",
       meta: [
